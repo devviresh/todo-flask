d2fb89de7ce91ec26d5124233514bd3b299e0fa9
devviresh <vireshdev5e@gmail.com>
2024-03-07 19:47:52 +0530

    database changed to postgres

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ import os
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('todo.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('todo.db')
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('/tmp', 'your_database_name.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://todo_viresh_user:HM7NV621RsocQ0yrkN7vGjXKKs0ve2Rp@dpg-cnknqrn109ks73d2q9ng-a.oregon-postgres.render.com/todo_viresh'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
 
 db = SQLAlchemy(app)
 
